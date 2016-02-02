b3c32fddeb025a3ea3947fe911110b2cf06b8dfd
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-02-02 16:09:50 +0000

    adding the responsearray

--- js/vanillaApp.js
+++ js/vanillaApp.js
@@ -2,9 +2,18 @@
    // this function takes the question object returned by the StackOverflow request
    // and returns new results to be appended to DOM
 var showQuestion = function(question) {
+  var xhr = new XMLHttpRequest();
+  var response;
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      response = JSON.parse(xhr.responseText);
 
-  
-    return result;
+    }
+  };
+  xhr.open("GET", "https://api.stackexchange.com/2.2/questions/unanswered?order=desc&sort=activity&site=stackoverflow", false);
+  xhr.send();
+  var result = response.items[0].link; // make loop
+  return result;
 };
 
 
@@ -17,10 +26,13 @@ var showSearchResults = function(query, resultNum) {
 
 
 // takes a string from input and searches
-// for unaswered questions on StackOverflow API. 
+// for unaswered questions on StackOverflow API.
 
-var getUnanswered = function(tags) {
+var getUnanswered = function(tags) {// inputs tags from submit and should match anything that we get from the showQuestion
+  // loop through elements
+  // if .match > -1 add response.item.title with link
 
+  return string // showSearchResultsesults ... list of links
 };
 
 
@@ -29,6 +41,5 @@ document.getElementById('unanswered-getter').addEventListener('submit',function(
 	  document.getElementById('results').innerHTML+="";
 	  var tags = document.getElementById('tags').value;
 	  getUnanswered(tags);
-
 
 });
---
+++ responseitemobject.js
@@ -0,0 +1,740 @@
+{
+items: [
+{
+tags: [
+"c++",
+"arrays",
+"c++11"
+],
+owner: {
+reputation: 494,
+user_id: 744184,
+user_type: "registered",
+accept_rate: 100,
+profile_image: "https://www.gravatar.com/avatar/b3c344835e7d6c28d221702306647db6?s=128&d=identicon&r=PG",
+display_name: "user000001",
+link: "http://stackoverflow.com/users/744184/user000001"
+},
+is_answered: false,
+view_count: 19,
+answer_count: 2,
+score: 0,
+last_activity_date: 1454422834,
+creation_date: 1454422520,
+question_id: 35155823,
+link: "http://stackoverflow.com/questions/35155823/why-i-do-not-get-errors-when-i-initilize-array-container-with-more-elemts-than-t",
+title: "Why I do not get errors when I initilize array container with more elemts than the size of the array?"
+},
+{
+tags: [
+"angularjs",
+"pagination",
+"httpservice"
+],
+owner: {
+reputation: 1,
+user_id: 5821081,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/91228961e16b6a94e143ce37d266433f?s=128&d=identicon&r=PG&f=1",
+display_name: "Bhushan",
+link: "http://stackoverflow.com/users/5821081/bhushan"
+},
+is_answered: false,
+view_count: 2,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422832,
+creation_date: 1454422832,
+question_id: 35155941,
+link: "http://stackoverflow.com/questions/35155941/first-page-data-not-showing-up-in-pagination",
+title: "First Page data not showing up in Pagination"
+},
+{
+tags: [
+"sql-server-2014-express"
+],
+owner: {
+reputation: 1,
+user_id: 4314272,
+user_type: "registered",
+profile_image: "https://i.stack.imgur.com/soVi3.jpg?s=128&g=1",
+display_name: "Mark Tang",
+link: "http://stackoverflow.com/users/4314272/mark-tang"
+},
+is_answered: false,
+view_count: 2,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422831,
+creation_date: 1454422831,
+question_id: 35155940,
+link: "http://stackoverflow.com/questions/35155940/whats-the-issues-sql-server-express-2014-installing-error-which-database-engin",
+title: "What&#39;s the issues, SQL Server Express 2014 installing error which database engine services are failed"
+},
+{
+tags: [
+"avd"
+],
+owner: {
+reputation: 1,
+user_id: 4391475,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/d6eb0ac2e121bb6198c356472c8410d8?s=128&d=identicon&r=PG&f=1",
+display_name: "Uday Modha",
+link: "http://stackoverflow.com/users/4391475/uday-modha"
+},
+is_answered: false,
+view_count: 3,
+answer_count: 0,
+score: -1,
+last_activity_date: 1454422830,
+creation_date: 1454421848,
+last_edit_date: 1454422830,
+question_id: 35155581,
+link: "http://stackoverflow.com/questions/35155581/unable-to-open-two-avd-in-android-studio",
+title: "Unable to open two avd in Android studio"
+},
+{
+tags: [
+"javascript",
+"jquery",
+"html"
+],
+owner: {
+reputation: 217,
+user_id: 4756141,
+user_type: "registered",
+accept_rate: 98,
+profile_image: "https://www.gravatar.com/avatar/044377903c875a1b4ad07cff57b7804e?s=128&d=identicon&r=PG&f=1",
+display_name: "99maas",
+link: "http://stackoverflow.com/users/4756141/99maas"
+},
+is_answered: false,
+view_count: 24,
+answer_count: 1,
+score: 0,
+last_activity_date: 1454422829,
+creation_date: 1454418058,
+question_id: 35154256,
+link: "http://stackoverflow.com/questions/35154256/how-to-collapse-expanded-rows-on-page-load",
+title: "How to collapse expanded rows on page Load"
+},
+{
+tags: [
+"c++",
+"algorithm",
+"stl",
+"time-complexity",
+"asymptotic-complexity"
+],
+owner: {
+reputation: 127,
+user_id: 5198101,
+user_type: "registered",
+accept_rate: 55,
+profile_image: "https://i.stack.imgur.com/hNo5i.jpg?s=128&g=1",
+display_name: "Richard George",
+link: "http://stackoverflow.com/users/5198101/richard-george"
+},
+is_answered: false,
+view_count: 13,
+answer_count: 1,
+score: 0,
+last_activity_date: 1454422828,
+creation_date: 1454422587,
+question_id: 35155846,
+link: "http://stackoverflow.com/questions/35155846/is-big-oh-the-only-notation-used-to-measure-complexity-in-stl",
+title: "Is Big Oh the only notation used to measure complexity in STL"
+},
+{
+tags: [
+"java"
+],
+owner: {
+reputation: 18,
+user_id: 5475395,
+user_type: "registered",
+accept_rate: 60,
+profile_image: "https://www.gravatar.com/avatar/779a8fe0275e939e9feb51521521132d?s=128&d=identicon&r=PG&f=1",
+display_name: "StudentX",
+link: "http://stackoverflow.com/users/5475395/studentx"
+},
+is_answered: false,
+view_count: 23,
+answer_count: 1,
+score: 1,
+last_activity_date: 1454422828,
+creation_date: 1454422223,
+question_id: 35155711,
+link: "http://stackoverflow.com/questions/35155711/splitting-the-results-of-a-regular-expression",
+title: "splitting the results of a regular expression"
+},
+{
+tags: [
+"j2objc"
+],
+owner: {
+reputation: 1,
+user_id: 5872928,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/94277c08647228a83e70bf00dd949406?s=128&d=identicon&r=PG&f=1",
+display_name: "L6Echo",
+link: "http://stackoverflow.com/users/5872928/l6echo"
+},
+is_answered: false,
+view_count: 2,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422826,
+creation_date: 1454422826,
+question_id: 35155937,
+link: "http://stackoverflow.com/questions/35155937/j2objc-httpurlconnection-cookiehandler",
+title: "j2objc + HttpURLConnection + CookieHandler"
+},
+{
+tags: [
+"java",
+"red5"
+],
+owner: {
+reputation: 1115,
+user_id: 863292,
+user_type: "registered",
+accept_rate: 89,
+profile_image: "https://www.gravatar.com/avatar/815e8ba5704fddbb88205820a6c8d741?s=128&d=identicon&r=PG",
+display_name: "Jovan",
+link: "http://stackoverflow.com/users/863292/jovan"
+},
+is_answered: false,
+view_count: 802,
+answer_count: 2,
+score: 1,
+last_activity_date: 1454422824,
+creation_date: 1324908730,
+question_id: 8636498,
+link: "http://stackoverflow.com/questions/8636498/red5-installation",
+title: "RED5 installation"
+},
+{
+tags: [
+"javascript",
+"jquery"
+],
+owner: {
+reputation: 37,
+user_id: 5563910,
+user_type: "registered",
+accept_rate: 71,
+profile_image: "https://www.gravatar.com/avatar/c6d37bfa5bd2e5bc8836d0397ce7136b?s=128&d=identicon&r=PG&f=1",
+display_name: "Bruno",
+link: "http://stackoverflow.com/users/5563910/bruno"
+},
+is_answered: false,
+view_count: 6,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422820,
+creation_date: 1454422820,
+question_id: 35155935,
+link: "http://stackoverflow.com/questions/35155935/jquery-applying-single-function-for-multiple-ids",
+title: "Jquery Applying single function for multiple IDs"
+},
+{
+tags: [
+"php"
+],
+owner: {
+reputation: 185,
+user_id: 3428369,
+user_type: "registered",
+accept_rate: 57,
+profile_image: "https://www.gravatar.com/avatar/521b35f170040b14f8bfb5c35dfa6718?s=128&d=identicon&r=PG",
+display_name: "messy",
+link: "http://stackoverflow.com/users/3428369/messy"
+},
+is_answered: false,
+view_count: 24,
+answer_count: 1,
+score: 2,
+last_activity_date: 1454422819,
+creation_date: 1454420515,
+question_id: 35155128,
+link: "http://stackoverflow.com/questions/35155128/array-to-string-conversion-exception-with-php-7",
+title: "Array to string conversion exception with PHP 7"
+},
+{
+tags: [
+"c#",
+"wpf",
+"combobox"
+],
+owner: {
+reputation: 31,
+user_id: 2695563,
+user_type: "registered",
+accept_rate: 56,
+profile_image: "https://www.gravatar.com/avatar/10b8b31323488e349eaa1417d611b3a6?s=128&d=identicon&r=PG&f=1",
+display_name: "Ralf",
+link: "http://stackoverflow.com/users/2695563/ralf"
+},
+is_answered: false,
+view_count: 5,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422816,
+creation_date: 1454421938,
+last_edit_date: 1454422816,
+question_id: 35155614,
+link: "http://stackoverflow.com/questions/35155614/gettemplatechild-returns-null-in-different-project",
+title: "GetTemplateChild returns null in different project"
+},
+{
+tags: [
+"excel",
+"excel-formula"
+],
+owner: {
+reputation: 1,
+user_id: 5490932,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/0e9ae19a923f7e3faf058b3de0f3c258?s=128&d=identicon&r=PG&f=1",
+display_name: "Patrick",
+link: "http://stackoverflow.com/users/5490932/patrick"
+},
+is_answered: false,
+view_count: 11,
+answer_count: 1,
+score: -1,
+last_activity_date: 1454422815,
+creation_date: 1454413892,
+question_id: 35152716,
+link: "http://stackoverflow.com/questions/35152716/excel-table-sorting-function",
+title: "Excel table sorting function"
+},
+{
+tags: [
+"jsf",
+"primefaces",
+"datatable"
+],
+owner: {
+reputation: 1,
+user_id: 5845910,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/06176ebb9e6ee71a78c82df872a7f4e4?s=128&d=identicon&r=PG&f=1",
+display_name: "KPK",
+link: "http://stackoverflow.com/users/5845910/kpk"
+},
+is_answered: false,
+view_count: 4,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422813,
+creation_date: 1454422211,
+last_edit_date: 1454422813,
+question_id: 35155705,
+link: "http://stackoverflow.com/questions/35155705/primefaces-initialize-columntoggler",
+title: "Primefaces initialize columnToggler"
+},
+{
+tags: [
+"javascript",
+"regex"
+],
+owner: {
+reputation: 16,
+user_id: 4466918,
+user_type: "registered",
+accept_rate: 0,
+profile_image: "https://www.gravatar.com/avatar/1c771c07398bc253aa8772496bfab5e3?s=128&d=identicon&r=PG&f=1",
+display_name: "msniezko",
+link: "http://stackoverflow.com/users/4466918/msniezko"
+},
+is_answered: false,
+view_count: 8,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422811,
+creation_date: 1454422811,
+question_id: 35155930,
+link: "http://stackoverflow.com/questions/35155930/regex-remove-all-occurences-of-a-word-from-the-string",
+title: "Regex - remove all occurences of a word from the string"
+},
+{
+tags: [
+"java",
+"android",
+"database",
+"android-notifications",
+"custom-notificaiton"
+],
+owner: {
+reputation: 55,
+user_id: 5855723,
+user_type: "registered",
+accept_rate: 100,
+profile_image: "https://www.gravatar.com/avatar/0202c7717d3aff3913e942aa03fe1b3a?s=128&d=identicon&r=PG&f=1",
+display_name: "Sid",
+link: "http://stackoverflow.com/users/5855723/sid"
+},
+is_answered: false,
+view_count: 3,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422810,
+creation_date: 1454422810,
+question_id: 35155929,
+link: "http://stackoverflow.com/questions/35155929/how-to-show-data-from-database-on-notification",
+title: "How to show data from database on notification?"
+},
+{
+tags: [
+"database",
+"postgresql"
+],
+owner: {
+reputation: 51,
+user_id: 384659,
+user_type: "registered",
+accept_rate: 56,
+profile_image: "https://www.gravatar.com/avatar/229aab4908917fae7ba3ca9fff3c8366?s=128&d=identicon&r=PG",
+display_name: "jamesclarke",
+link: "http://stackoverflow.com/users/384659/jamesclarke"
+},
+is_answered: false,
+view_count: 2,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422804,
+creation_date: 1454422804,
+question_id: 35155928,
+link: "http://stackoverflow.com/questions/35155928/postgres-trigger-database-connection-release-time",
+title: "Postgres trigger database connection release time"
+},
+{
+tags: [
+"c#",
+"mobile",
+"xamarin",
+"windows-phone-8.1",
+"xamarin.forms"
+],
+owner: {
+reputation: 220,
+user_id: 618782,
+user_type: "registered",
+accept_rate: 77,
+profile_image: "https://www.gravatar.com/avatar/3b07ad65f133eaf69feba4c6a9b2c508?s=128&d=identicon&r=PG",
+display_name: "Lord-Link",
+link: "http://stackoverflow.com/users/618782/lord-link"
+},
+is_answered: false,
+view_count: 3,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422804,
+creation_date: 1454422804,
+question_id: 35155926,
+link: "http://stackoverflow.com/questions/35155926/custom-picker-renderer-draw-event-not-recognized-in-xamarin-forms-for-winphone",
+title: "Custom picker renderer draw event not recognized in Xamarin.Forms for WinPhone"
+},
+{
+tags: [
+"ios",
+"objective-c",
+"iphone",
+"xcode"
+],
+owner: {
+reputation: 1776,
+user_id: 1280188,
+user_type: "registered",
+accept_rate: 52,
+profile_image: "https://www.gravatar.com/avatar/50d3ad4255590318ae3c8dab6dac6e10?s=128&d=identicon&r=PG",
+display_name: "iphonemaclover",
+link: "http://stackoverflow.com/users/1280188/iphonemaclover"
+},
+is_answered: false,
+view_count: 11,
+answer_count: 1,
+score: 0,
+last_activity_date: 1454422804,
+creation_date: 1454422273,
+question_id: 35155727,
+link: "http://stackoverflow.com/questions/35155727/what-is-the-architecture-set-for-all-device-working",
+title: "what is the architecture set for all device working"
+},
+{
+tags: [
+"excel",
+"if-statement",
+"excel-2010"
+],
+owner: {
+reputation: 5,
+user_id: 5517141,
+user_type: "registered",
+profile_image: "https://graph.facebook.com/1506510053008643/picture?type=large",
+display_name: "Nico",
+link: "http://stackoverflow.com/users/5517141/nico"
+},
+is_answered: false,
+view_count: 12,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422800,
+creation_date: 1454421955,
+last_edit_date: 1454422800,
+question_id: 35155621,
+link: "http://stackoverflow.com/questions/35155621/andor-function-not-working-as-supposed-to",
+title: "AND+OR function not working as supposed to"
+},
+{
+tags: [
+"sql",
+"date"
+],
+owner: {
+reputation: 1,
+user_id: 5873053,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/7dd801fd2250ed0a0751a3a3649814dd?s=128&d=identicon&r=PG&f=1",
+display_name: "Jente",
+link: "http://stackoverflow.com/users/5873053/jente"
+},
+is_answered: false,
+view_count: 19,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422795,
+creation_date: 1454422404,
+last_edit_date: 1454422795,
+question_id: 35155772,
+link: "http://stackoverflow.com/questions/35155772/grouping-dates-for-each-period",
+title: "Grouping dates for each period"
+},
+{
+tags: [
+"c",
+"arrays"
+],
+owner: {
+reputation: 34,
+user_id: 5857261,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/8d15207627f8c2d685753e72471af7ba?s=128&d=identicon&r=PG&f=1",
+display_name: "sam",
+link: "http://stackoverflow.com/users/5857261/sam"
+},
+is_answered: true,
+view_count: 6,
+answer_count: 1,
+score: 0,
+last_activity_date: 1454422865,
+creation_date: 1454422794,
+question_id: 35155924,
+link: "http://stackoverflow.com/questions/35155924/fastest-format-to-print-an-array",
+title: "Fastest format to print an array"
+},
+{
+tags: [
+"applescript"
+],
+owner: {
+reputation: 58,
+user_id: 5330708,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/31150cc3f72fcfff9dea1fcd77c3d922?s=128&d=identicon&r=PG&f=1",
+display_name: "Kamin Pallaghy",
+link: "http://stackoverflow.com/users/5330708/kamin-pallaghy"
+},
+is_answered: false,
+view_count: 2,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422790,
+creation_date: 1454422790,
+question_id: 35155923,
+link: "http://stackoverflow.com/questions/35155923/applescript-records-get-unknown-property",
+title: "Applescript Records get unknown property"
+},
+{
+tags: [
+"solr",
+"limit",
+"restrict",
+"wcs"
+],
+owner: {
+reputation: 1,
+user_id: 5868485,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/e914682c9e27bf0ccf16137c131ca968?s=128&d=identicon&r=PG&f=1",
+display_name: "Anand S P",
+link: "http://stackoverflow.com/users/5868485/anand-s-p"
+},
+is_answered: false,
+view_count: 7,
+answer_count: 1,
+score: 0,
+last_activity_date: 1454422790,
+creation_date: 1454341385,
+question_id: 35134506,
+link: "http://stackoverflow.com/questions/35134506/restrict-solr-search-results-in-ibm-wcs-fep6",
+title: "Restrict solr search results in ibm WCS FEP6"
+},
+{
+tags: [
+"codeigniter",
+"mysqli",
+"server"
+],
+owner: {
+reputation: 15,
+user_id: 3004315,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/a72e5b97b6b7d9c9202df060295d3154?s=128&d=identicon&r=PG&f=1",
+display_name: "Bsos",
+link: "http://stackoverflow.com/users/3004315/bsos"
+},
+is_answered: false,
+view_count: 1088,
+bounty_amount: 50,
+bounty_closes_date: 1455004723,
+answer_count: 1,
+score: 1,
+last_activity_date: 1454422789,
+creation_date: 1447084311,
+last_edit_date: 1447085153,
+question_id: 33612956,
+link: "http://stackoverflow.com/questions/33612956/codeigniter-fatal-error-call-to-undefined-function-mysqli-init",
+title: "Codeigniter: fatal error call to undefined function mysqli_init()"
+},
+{
+tags: [
+"osx",
+"bootcamp"
+],
+owner: {
+reputation: 3,
+user_id: 4938051,
+user_type: "registered",
+profile_image: "https://lh3.googleusercontent.com/-4GyqYbFo0sI/AAAAAAAAAAI/AAAAAAAAADI/CQMSIkLxDsc/photo.jpg?sz=128",
+display_name: "yoloswagg27",
+link: "http://stackoverflow.com/users/4938051/yoloswagg27"
+},
+is_answered: false,
+view_count: 3,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422788,
+creation_date: 1454422788,
+question_id: 35155921,
+link: "http://stackoverflow.com/questions/35155921/os-x-boot-camp-no-windows-boot-option-after-installation",
+title: "OS X Boot Camp No Windows boot option after installation"
+},
+{
+tags: [
+"testng",
+"allure"
+],
+owner: {
+reputation: 1,
+user_id: 5872174,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/16aabb6875e11fb16b8dfd3ba88d64b7?s=128&d=identicon&r=PG&f=1",
+display_name: "abrakadabrda",
+link: "http://stackoverflow.com/users/5872174/abrakadabrda"
+},
+is_answered: false,
+view_count: 17,
+answer_count: 1,
+score: 0,
+last_activity_date: 1454422787,
+creation_date: 1454410235,
+last_edit_date: 1454411455,
+question_id: 35151399,
+link: "http://stackoverflow.com/questions/35151399/how-to-convert-xml-to-html-using-allure-report-framework",
+title: "How to convert xml to html using allure report framework"
+},
+{
+tags: [
+"entity-framework-6",
+"nopcommerce",
+"glimpse"
+],
+owner: {
+reputation: 1,
+user_id: 5872668,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/e4501fe48b117a26b6ec5eaa17ef28f7?s=128&d=identicon&r=PG&f=1",
+display_name: "Naresh Devabatini",
+link: "http://stackoverflow.com/users/5872668/naresh-devabatini"
+},
+is_answered: false,
+view_count: 5,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422786,
+creation_date: 1454416895,
+last_edit_date: 1454422786,
+question_id: 35153822,
+link: "http://stackoverflow.com/questions/35153822/glimpsesql-tab-is-not-working-for-nopcommerce",
+title: "glimpse(sql tab) is not working for nopcommerce"
+},
+{
+tags: [
+"cocos2d-x",
+"sprite",
+"cocos2d-js"
+],
+owner: {
+reputation: 8,
+user_id: 3599940,
+user_type: "registered",
+profile_image: "https://graph.facebook.com/1313870731/picture?type=large",
+display_name: "joonor23",
+link: "http://stackoverflow.com/users/3599940/joonor23"
+},
+is_answered: false,
+view_count: 2,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422784,
+creation_date: 1454422303,
+last_edit_date: 1454422784,
+question_id: 35155738,
+link: "http://stackoverflow.com/questions/35155738/cocos2d-trying-to-keep-sprites-within-the-screen",
+title: "cocos2d trying to keep sprites within the screen"
+},
+{
+tags: [
+"javascript",
+"php",
+"jquery",
+"iframe",
+"response"
+],
+owner: {
+reputation: 1,
+user_id: 5646711,
+user_type: "registered",
+profile_image: "https://www.gravatar.com/avatar/d317ce1fda3b293d3c74e05ac7fe6773?s=128&d=identicon&r=PG&f=1",
+display_name: "RincewindAssoc",
+link: "http://stackoverflow.com/users/5646711/rincewindassoc"
+},
+is_answered: false,
+view_count: 5,
+answer_count: 0,
+score: 0,
+last_activity_date: 1454422783,
+creation_date: 1454422783,
+question_id: 35155920,
+link: "http://stackoverflow.com/questions/35155920/html-form-response-in-iframe-call-on-the-parent-page",
+title: "html-form response in iframe call on the parent page"
+}
+],
+has_more: true,
+quota_max: 300,
+quota_remaining: 100
+}
